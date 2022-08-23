a61919df669397fe3358cca2bc99810b4243298f
User <user@Users-MacBook-Pro.local>
2022-08-23 23:20:06 +0600

    view publish

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -13,6 +13,7 @@ class ContactServiceProvider extends ServiceProvider{
         );
         $this->publishes([
             __DIR__.'/config/contact.php' => config_path('contact.php'),
+            __DIR__.'/resources/views' => resource_path('views/vendor/contact'),
         ]);
     }
     public function register(){}
